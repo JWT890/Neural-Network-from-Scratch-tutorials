5d33b0d472de6bbd66c6129d6419420c7a895e8c
Jon William Taylor <jwtaylor659@gmail.com>
2023-11-29 09:33:40 -0600

    Update

--- YouTube tutorial 1/activations.py
+++ YouTube tutorial 1/activations.py
@@ -1,4 +1,5 @@
 from activation import Activation
+from layer import Layer
 import numpy as np
 
 class Tanh(Activation):
@@ -6,3 +7,24 @@ class Tanh(Activation):
         tanh = lambda x: np.tanh(x)
         tanh_prime = lambda x: 1 - np.tanh(x) ** 2
         super().__init__(tanh, tanh_prime)
+
+class Sigmoid(Activation):
+    def __init__(self):
+        def sigmoid(x):
+            return 1 / (1 + np.exp(-x))
+        
+        def sigmoid_prime(x):
+            s = sigmoid(x)
+            return s * (1 - s)
+        
+        super().__init__(sigmoid, sigmoid_prime)
+
+class Softmax(Layer):
+    def forward(self, input):
+        tmp = np.exp(input)
+        self.output = tmp / np.sum(tmp)
+        return self.output
+    
+    def backward(self, output_gradient, learning_rate):
+        n = np.size(self.output)
+        return np.dot((np.identity(n) - self.output.T) * self.output, output_gradient)
